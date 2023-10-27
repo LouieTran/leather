a97856e283398582c4902f27bdbf6f3f867597eb
duytd <duytd@thinhtientech.com>
2023-10-27 17:46:56 +0700

    init project

---
+++ app.js
@@ -0,0 +1,18 @@
+// import express from 'express';
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World1!');
+});
+
+app.listen(port, () => {
+  console.log(`Express running → PORT ${port}`);
+});
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
